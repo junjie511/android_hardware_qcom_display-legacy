2f11dc46f705b0a6cde8ad44686d1fa57d887f9f
enoodle <junjie511@gmail.com>
2013-11-03 12:47:31 +0800

    backup for kernel-3.4 of aosp4.3
    Change-Id: Ic0dc9dae44515f027bde7a5bbae9d107ae95c584

--- common.mk
+++ common.mk
@@ -1,9 +1,9 @@
 #Common headers
-common_includes := hardware/qcom/display-caf-legacy/libgralloc
-common_includes += hardware/qcom/display-caf-legacy/libgenlock
-common_includes += hardware/qcom/display-caf-legacy/liboverlay
-common_includes += hardware/qcom/display-caf-legacy/libcopybit
-common_includes += hardware/qcom/display-caf-legacy/libqdutils
+common_includes := hardware/qcom/display-legacy/libgralloc
+common_includes += hardware/qcom/display-legacy/libgenlock
+common_includes += hardware/qcom/display-legacy/liboverlay
+common_includes += hardware/qcom/display-legacy/libcopybit
+common_includes += hardware/qcom/display-legacy/libqdutils
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_flags     += -DUSES_POST_PROCESSING
@@ -16,7 +16,7 @@ common_libs := liblog libutils libcutils libhardware
 
 #Common C flags
 common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
-common_flags += -Werror
+#common_flags += -Werror
 
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
